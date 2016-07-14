efe6e272bde1cf806de762b921668417999887be
m4n3dw0lf <m4n3dw0lf@gmail.com>
2016-07-14 20:04:31 -0300

    TCP fuzzer improved :)

--- modules/fuzzer.py
+++ modules/fuzzer.py
@@ -26,11 +26,12 @@ import resource
 import time
 from netaddr import IPAddress, AddrFormatError
 from subprocess import *
-from socket import *
+import socket
 
 class SimpleFuzz(object):
 
-	def __init__(self, target, type):
+	def __init__(self, target, type, offset):
+		self.offset = offset
 		self.target = target
 		if type == "tcp":
 			self.port = input("[+]Enter the tcp port to fuzz: ")
@@ -46,12 +47,12 @@ class SimpleFuzz(object):
 		while True:
 			try:
 				first = True
-				buf += '\x41'
+				buf += '\x41' * self.offset
 				resource.setrlimit(resource.RLIMIT_STACK, (-1, -1))
 				resource.setrlimit(resource.RLIMIT_CORE, (-1, -1))
 				P = Popen(self.target, stdin=PIPE)
 				print "[*] Sending buffer with lenght: "+str(len(buf))
-				P.stdin.write(buf+ "\n")
+				P.stdin.write(buf)
 				line = sys.stdin.readline()
 				P.poll()
 				ret = P.returncode
@@ -87,26 +88,51 @@ class SimpleFuzz(object):
 		except AddrFormatError as e:
 			print "[-] Select a valid IP Address as target."
 			return
+
+		buf = '\x41'*self.offset
+		print "[+] TCP fuzzing initialized, wait untill crash."
 		while True:
 			try:
 				self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-				self.socket.settimeout(2)
+				self.socket.settimeout(1)
 				self.socket.connect((self.target,self.port))
-				#self.socket.recv(1024)
-				buf = '\x41'
-				print "[+] TCP fuzzing initialized, wait untill crash."
-				while True:
+				print "[+] Fuzzing with [{}] bytes.".format(len(buf))
+				try:
+					response = self.socket.recv(1024)
+					print "[*] Response: {}".format(response)
 					self.socket.send(buf)
-					buf += '\x41'
-				self.socket.recv(1024)
+					try:
+						response = self.socket.recv(1024)
+						print "[*] Response: {}".format(response)
+						self.socket.close()
+						time.sleep(1)
+						buf += '\x41'*self.offset
+					except:
+						self.socket.close()
+						time.sleep(1)
+						buf += '\x41'*self.offset
+				except:
+					self.socket.send(buf)
+					try:
+						response = self.socket.recv(1024)
+						print "[*] Response: {}".format(response)
+						self.socket.close()
+						time.sleep(1)
+						buf += '\x41'*self.offset
+
+					except:
+						self.socket.close()
+						time.sleep(1)
+						buf += '\x41'*self.offset
 
 			except KeyboardInterrupt:
 				break
 			except Exception as e:
 				if 'Connection refused' in e:
 					print "[-] Connection refused."
-					break
+					time.sleep(5)
 				else:
-					print "[+] Crash occured with buffer length: {}".format(str(len(buf)))
-					break
-
+					try:
+						self.socket.recv(1024)
+					except:
+						print "[+] Crash occured with buffer length: {}".format(str(len(buf)))
